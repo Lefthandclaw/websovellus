7799416516609a229e67df7efbeb9b2f8d2754a2
motha <mothaops69@gmail.com>
2025-01-22 19:49:26 +0200

    websovellus

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
